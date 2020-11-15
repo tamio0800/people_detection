c9a9eb040b2699760ccf291f497aa2c66a9b1656
tamio <tamio.chou@gmail.com>
2020-11-16 01:42:30 +0800

    Done video capture test and yolo_video_dection

---
+++ test_webcam.py
@@ -0,0 +1,18 @@
+import cv2
+# 選擇第二隻攝影機
+
+cap = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT , 768)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH , 1024)
+while(True):
+  # 從攝影機擷取一張影像
+  ret, frame = cap.read()
+  # 顯示圖片
+  cv2.imshow('frame', frame)
+  # 若按下 q 鍵則離開迴圈
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+# 釋放攝影機
+cap.release()
+# 關閉所有 OpenCV 視窗
+cv2.destroyAllWindows()
